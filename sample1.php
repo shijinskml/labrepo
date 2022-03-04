38df4e36a9dac241df3f383602da74eab4e0b69c
SHIGIN S <85169428+shijinskml@users.noreply.github.com>
2022-03-04 12:22:30 +0530

    Add files via upload

---
+++ sample1.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form method="post" action="display.php">
+name:
+<input type="text" name="a">
+<br>AGE:<input type="text" name="b">
+<br>pass:<input type="password" name="c">
+<br><input type="submit" value="submit" name="submit">
+</form>
+
+</body>
+</html>
+
---
+++ sconn.php
@@ -0,0 +1,9 @@
+<html>
+<?php
+$conn=mysqli_connect("localhost","root","cetmca","sample");
+if($conn)
+{
+ echo "Connected";
+}
+?>
+</html>
